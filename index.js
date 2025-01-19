4bc59af066be5e39f4b6b5720d65a517798098d2
irinavladimirovna1307 <magstroiardi@gmail.com>
2025-01-19 04:12:46 +0300

    .

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 function calculateTotalPrice(quantity = 2, price = 15000000) {
-    const totalPrice = quantity * price;
-     // Использования DOM
-    const formattedPrice = totalPrice.toLocaleString('ru-RU');
-    document.getElementById('totalPriceDisplay').innerText = `Стоимость покупки: ${formattedPrice} рублей`;
-    
-    // // Вместо использования DOM, мы выводим результат в консоль или через alert
-    // alert(`Стоимость покупки: ${totalPrice} рублей`);
-    // console.log(`Стоимость покупки: ${totalPrice} рублей`);
+  const totalPrice = quantity * price;
+  // Использования DOM
+  const formattedPrice = totalPrice.toLocaleString("ru-RU");
+  document.getElementById(
+    "totalPriceDisplay"
+  ).innerText = `Стоимость покупки: ${formattedPrice} рублей`;
+
+  // // Вместо использования DOM, мы выводим результат в консоль или через alert
+  // alert(`Стоимость покупки: ${totalPrice} рублей`);
+  // console.log(`Стоимость покупки: ${totalPrice} рублей`);
 }
